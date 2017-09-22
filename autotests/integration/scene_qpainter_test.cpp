18548df5a22f04f5a0e3817e1ef40f0a061f883a
David Edmundson <kde@davidedmundson.co.uk>
2017-09-22 14:52:21 +0100

    Update tests to cover XDGv6

--- autotests/integration/scene_qpainter_test.cpp
+++ autotests/integration/scene_qpainter_test.cpp
@@ -151,6 +151,7 @@ void SceneQPainterTest::testWindow_data()
 
     QTest::newRow("wlShell") << Test::ShellSurfaceType::WlShell;
     QTest::newRow("xdgShellV5") << Test::ShellSurfaceType::XdgShellV5;
+    QTest::newRow("xdgShellV6") << Test::ShellSurfaceType::XdgShellV6;
 }
 
 void SceneQPainterTest::testWindow()
@@ -255,6 +256,7 @@ void SceneQPainterTest::testCompositorRestart_data()
 
     QTest::newRow("wlShell") << Test::ShellSurfaceType::WlShell;
     QTest::newRow("xdgShellV5") << Test::ShellSurfaceType::XdgShellV5;
+    QTest::newRow("xdgShellV6") << Test::ShellSurfaceType::XdgShellV6;
 }
 
 void SceneQPainterTest::testCompositorRestart()
